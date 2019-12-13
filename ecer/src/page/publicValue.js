549a2cd5879056449e764cba293e70d15b693873
rq <17855337639@163.com>
2019-12-13 17:50:45 +0800

    9

--- ecer/src/page/publicValue.js
+++ ecer/src/page/publicValue.js
@@ -1,10 +1,11 @@
 import Vue from 'vue'
 // var api ="http://172.16.211.152"
 var roles ="1"
+var api = "http://172.16.211.75:8080/"
 
 export default {
     install () {
         Vue.prototype.roles = roles
-        // Vue.prototype.api1 = api1
+        Vue.prototype.api = api
     }
 }
